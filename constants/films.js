34b85167ac4177705e5ae8864f855bc9f726c284
= <abderrahmanne.bihi@zokas.tech>
2024-06-27 11:25:54 +0100

    couldinary

--- constants/films.js
+++ constants/films.js
@@ -1,38 +1,38 @@
 export const FILMS = [
     {
-        img: "/./Films/1.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483483/1_sxn6ly.jpg",
         name: "Trigger Warning",
       },
       {
-        img: "/./Films/11.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483485/11_skgnvt.jpg",
         name: "Bridgerton",
       },
       {
-        img: "/./Films/2.webp",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483483/2_rngyki.webp",
         name: "Thanksgiving",
       },
       {
-        img: "/./Films/22.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483483/22_p1u6v6.jpg",
         name: "Raising Voices",
       },
       {
-        img: "/./Films/3.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483483/3_je2pne.jpg",
         name: "Inheritance",
       },
       {
-        img: "/./Films/33.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483484/33_buibr9.jpg",
         name: "Geek Girl",
       },
       {
-        img: "/./Films/4.jpg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483484/4_lmp1ae.jpg",
         name: "Get Out",
       },
       {
-        img: "/./Films/44.webp",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483484/44_eghti7.webp",
         name: "Sweet Tooth",
       },
       {
-        img: "/./Films/5.jpeg",
+        img: "https://res.cloudinary.com/dyzdrbivf/image/upload/v1719483483/5_tpdwae.jpg",
         name: "Under Paris",
       }
 ]
