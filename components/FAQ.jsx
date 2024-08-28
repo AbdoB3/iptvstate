7273b81783ac45b8adbf2fd9c0f3a3ee0b6b737f
= <abderrahmanne.bihi@zokas.tech>
2024-08-28 21:11:25 +0100

    faq content

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -47,29 +47,30 @@ const FaqsCard = ({ faqsList }) => {
 }
 
 export default function Faqs() {
+    
+    const t = useTranslations('Faq');
     const faqsList = [
         {
-            q: "What are some random questions to ask?",
-            a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question."
+            q: t('content.first.title'),
+            a: t('content.first.desc')
         },
         {
-            q: "Do you include common questions?",
-            a: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator."
+            q: t('content.second.title'),
+            a: t('content.second.desc')
         },
         {
-            q: "Can I use this for 21 questions?",
-            a: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated."
+            q: t('content.third.title'),
+            a: t('content.third.desc')
         },
         {
-            q: "Are these questions for girls or for boys?",
-            a: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with)."
+            q:  t('content.fourth.title'),
+            a: t('content.fourth.desc')
         },
         {
-            q: "What do you wish you had more talent doing?",
-            a: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires."
+            q:  t('content.fifth.title'),
+            a: t('content.fifth.desc')
         }
     ]
-    const t = useTranslations('Faq');
   
     return (
         <section className="leading-relaxed max-w-screen-xl mt-12 mx-auto px-4 md:px-8">
