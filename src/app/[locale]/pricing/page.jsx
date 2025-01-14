f110fa8f8dabd5d156110e8c3f30350a7afdf891
= <R139591066@taalim.ma>
2025-01-14 23:52:09 +0100

    re adding metadata to pricing page

--- src/app/[locale]/pricing/page.jsx
+++ src/app/[locale]/pricing/page.jsx
@@ -1,13 +1,22 @@
 import HowItWorks from "@/components/home/Steps";
 import PricingT from '@/components/pricing/PricingT';
+import { getMessages } from 'next-intl/server';
 
+
+export async function generateMetadata({ params: { locale } }) {
+  const messages = await getMessages({ locale });
+  const title = messages.Navbar.pricing_page;
+  return {
+    title,
+  };
+}
 
 
 export default function Home() {
   return (
     <>
-      <HowItWorks/>
-        <PricingT />
+      <HowItWorks />
+      <PricingT />
     </>
 
   );
